adb2691b04dd716a3c9f9b52bcb63e716457ae40
Erik Nomitch <enomitch@gmail.com>
2018-04-11 14:37:58 -0500

    Set notebook dir to /shared

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -211,7 +211,7 @@ c.NotebookApp.ip = '*'
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-#c.NotebookApp.notebook_dir = ''
+c.NotebookApp.notebook_dir = '/shared'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
