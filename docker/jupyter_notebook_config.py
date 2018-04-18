861d9fa3f08ff59cbb4f4598e02ff0ea934814eb
Erik Nomitch <enomitch@gmail.com>
2018-04-18 14:38:00 -0500

    Switch notebook_dir to shared

--- docker/jupyter_notebook_config.py
+++ docker/jupyter_notebook_config.py
@@ -211,7 +211,7 @@ c.NotebookApp.ip = '*'
 #c.NotebookApp.nbserver_extensions = {}
 
 ## The directory to use for notebooks and kernels.
-c.NotebookApp.notebook_dir = '/volumes/notebooks'
+c.NotebookApp.notebook_dir = '/shared/notebooks'
 
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
