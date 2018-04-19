6d799d65482f12907a95f4fc0703d6e0b94f8c12
Erik Nomitch <enomitch@gmail.com>
2018-04-19 14:34:11 -0500

    - $

--- docker/install_from_config.py
+++ docker/install_from_config.py
@@ -4,17 +4,17 @@ from os import system
 #  UTILITY->INSTALL ------------------------------
 #  -----------------------------------------------
 def install_from_config(config_filename, format_syscall_fn):
-  for name in open(f"/root/config/${config_filename}").read().splitlines():
+  for name in open(f"/root/config/{config_filename}").read().splitlines():
     system(format_syscall_fn(name))
 
 #  ===============================================
 #  MAIN ==========================================
 #  ===============================================
 install_from_config("apt",
-                    lambda name: f"apt-get install -y ${name}")
+                    lambda name: f"apt-get install -y {name}")
 install_from_config("pip",
-                    lambda name: f"pip install ${name}")
+                    lambda name: f"pip install {name}")
 install_from_config("jupyter",
-                    lambda name: f"jupyter nbextension enable ${name}")
+                    lambda name: f"jupyter nbextension enable {name}")
 install_from_config("jupyterlab",
-                    lambda name: f"jupyter labextension install ${name}")
+                    lambda name: f"jupyter labextension install {name}")
