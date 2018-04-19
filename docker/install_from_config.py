e97edaa6b62128f511f39b793682bb1c0f6a8d2b
Erik Nomitch <enomitch@gmail.com>
2018-04-19 14:33:13 -0500

    Add more config/ installs

--- docker/install_from_config.py
+++ docker/install_from_config.py
@@ -1,5 +1,20 @@
-import os
+from os import system
 
-for package in open("/root/config/apt").read().splitlines():
-  os.system(f"apt-get install -y ${package}")
+#  -----------------------------------------------
+#  UTILITY->INSTALL ------------------------------
+#  -----------------------------------------------
+def install_from_config(config_filename, format_syscall_fn):
+  for name in open(f"/root/config/${config_filename}").read().splitlines():
+    system(format_syscall_fn(name))
 
+#  ===============================================
+#  MAIN ==========================================
+#  ===============================================
+install_from_config("apt",
+                    lambda name: f"apt-get install -y ${name}")
+install_from_config("pip",
+                    lambda name: f"pip install ${name}")
+install_from_config("jupyter",
+                    lambda name: f"jupyter nbextension enable ${name}")
+install_from_config("jupyterlab",
+                    lambda name: f"jupyter labextension install ${name}")
