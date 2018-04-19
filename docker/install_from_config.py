6648b769af4eb59eeb9d5b428aae521ed3ec96c2
Erik Nomitch <enomitch@gmail.com>
2018-04-19 14:50:31 -0500

    Upgrade pip; fix ENV

--- docker/install_from_config.py
+++ docker/install_from_config.py
@@ -1,19 +1,21 @@
-from os import system
+import os
 
-#  -----------------------------------------------
-#  UTILITY->INSTALL ------------------------------
-#  -----------------------------------------------
+# ------------------------------------------------
+# UTILITY->INSTALL -------------------------------
+# ------------------------------------------------
 def install_from_config(config_filename, format_syscall_fn):
   for name in open(f"/root/config/{config_filename}").read().splitlines():
-    system(format_syscall_fn(name))
+    os.system(format_syscall_fn(name))
 
-#  ===============================================
-#  MAIN ==========================================
-#  ===============================================
+# ================================================
+# MAIN ===========================================
+# ================================================
 install_from_config("apt",
-                    lambda name: f"apt-get install -y {name}")
+                    lambda name: f"apt-get install -y --no-install-recommends {name}")
 install_from_config("pip",
-                    lambda name: f"pip install {name}")
+                    lambda name: f"pip --no-cache-dir install --upgrade {name}")
+install_from_config("jupyter",
+                    lambda name: f"jupyter nbextension install {name}")
 install_from_config("jupyter",
                     lambda name: f"jupyter nbextension enable {name}")
 install_from_config("jupyterlab",
