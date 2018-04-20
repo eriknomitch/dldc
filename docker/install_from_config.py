b5d2aac0dbe31eed03b88ff5562b35b8ee4090f7
Erik Nomitch <enomitch@gmail.com>
2018-04-20 12:15:25 -0500

    + main sentinel

--- docker/install_from_config.py
+++ docker/install_from_config.py
@@ -1,7 +1,16 @@
+# ================================================
+# PYTHON->INSTALL-FROM-CONFIG ====================
+# ================================================
+# This file is copied to the Docker image during build and run on it to install
+# packages into the image.
+
+# ------------------------------------------------
+# IMPORT -----------------------------------------
+# ------------------------------------------------
 import os
 
 # ------------------------------------------------
-# UTILITY->INSTALL -------------------------------
+# UTILITY ----------------------------------------
 # ------------------------------------------------
 def install_from_config(config_filename, format_syscall_fn):
   for name in open(f"/root/config/{config_filename}").read().splitlines():
@@ -11,11 +20,15 @@ def install_from_config(config_filename, format_syscall_fn):
 # ================================================
 # MAIN ===========================================
 # ================================================
-install_from_config("apt",
-                    lambda name: f"apt-get install -y --no-install-recommends {name}")
-install_from_config("pip",
-                    lambda name: f"pip --no-cache-dir install --upgrade {name}")
-# install_from_config("jupyter",
-#                     lambda name: f"jupyter nbextension enable {name}")
-install_from_config("jupyterlab",
-                    lambda name: f"jupyter labextension install {name}")
+def main():
+    install_from_config("apt",
+                        lambda name: f"apt-get install -y --no-install-recommends {name}")
+    install_from_config("pip",
+                        lambda name: f"pip --no-cache-dir install --upgrade {name}")
+    install_from_config("jupyter",
+                        lambda name: f"jupyter nbextension enable {name}")
+    install_from_config("jupyterlab",
+                        lambda name: f"jupyter labextension install {name}")
+
+if __name__ == "__main__":
+    main()
