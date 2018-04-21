9520db3bfd002332f6e57d06fdd0b6109ee699c3
Erik Nomitch <enomitch@gmail.com>
2018-04-21 00:18:33 -0500

    + noninteractive

--- docker/scripts/install_from_config.py
+++ docker/scripts/install_from_config.py
@@ -1,8 +1,12 @@
 # ================================================
 # PYTHON->INSTALL-FROM-CONFIG ====================
 # ================================================
+
 # This file is copied to the Docker image during build and run on it to install
 # packages into the image.
+#
+# This allows the user to quickly add in packages they need to configure their own
+# dldc image (which builds on ./dldc).
 
 # ------------------------------------------------
 # IMPORT -----------------------------------------
@@ -21,6 +25,9 @@ def install_from_config(config_filename, format_syscall_fn):
 # MAIN ===========================================
 # ================================================
 def main():
+
+    os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
+
     install_from_config("apt",
                         lambda name: f"apt-get install -y --no-install-recommends {name}")
     install_from_config("pip",
