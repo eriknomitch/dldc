0fed0c63a79c4df8a45898483f0b9cfcde21b125
Erik Nomitch <enomitch@gmail.com>
2018-04-26 15:31:46 -0500

    Add specific subsystem installs for install_from_config

--- docker/scripts/install_from_config.py
+++ docker/scripts/install_from_config.py
@@ -13,6 +13,7 @@
 # ------------------------------------------------
 import os
 from shlex import quote
+import argparse
 
 # ------------------------------------------------
 # UTILITY ----------------------------------------
@@ -22,23 +23,49 @@ def install_from_config(config_filename, format_syscall_fn):
     if name != "":
         os.system(format_syscall_fn(quote(name)))
 
-# ================================================
-# MAIN ===========================================
-# ================================================
-def main():
-
+# ------------------------------------------------
+# SUBSYSTEMS -------------------------------------
+# ------------------------------------------------
+def subsystem_apt():
     os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
 
     install_from_config("apt",
                         lambda name: f"apt-get install -y --no-install-recommends {name}")
+
+def subsystem_lua():
     install_from_config("lua",
                         lambda name: f"luarocks install {name}")
+
+def subsystem_jupyter():
     install_from_config("jupyter",
                         lambda name: f"jupyter nbextension enable {name}")
+
+def subsystem_jupyterlab():
     install_from_config("jupyterlab",
                         lambda name: f"jupyter labextension install {name}")
+def subsystem_pip():
     install_from_config("pip",
                         lambda name: f"pip --no-cache-dir install --upgrade {name}")
+
+# ================================================
+# MAIN ===========================================
+# ================================================
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("subsystem")
+
+    args = parser.parse_args()
+
+    subsystems = {
+        'apt': subsystem_apt,
+        'lua': subsystem_lua,
+        'jupyter': subsystem_jupyter,
+        'jupyterlab': subsystem_jupyterlab,
+        'pip': subsystem_pip
+    }
+
+    options[args.subsystem]()
 
 if __name__ == "__main__":
     main()
