4a7f07dccef79597224c46026bfb25255278d343
Erik Nomitch <enomitch@gmail.com>
2018-06-18 19:40:02 +0000

    Pass user_defined

--- docker/scripts/install_from_config.py
+++ docker/scripts/install_from_config.py
@@ -19,36 +19,50 @@ import argparse
 # UTILITY ----------------------------------------
 # ------------------------------------------------
 def install_from_config(config_filename, format_syscall_fn, user_defined=False):
-  directory = 'packages' if user_defined else 'packages-core'
+    directory = "packages" if user_defined else "packages-core"
 
-  for line in open(f'/root/.config-image/{directory}/{config_filename}').read().splitlines():
-    # Omit empty lines and quoted lines
-    if line != '' and not line.startswith('#'):
-        os.system(format_syscall_fn(quote(line)))
+    for line in (
+        open(f"/root/.config-image/{directory}/{config_filename}").read().splitlines()
+    ):
+        # Omit empty lines and quoted lines
+        if line != "" and not line.startswith("#"):
+            os.system(format_syscall_fn(quote(line)))
+
 
 # ------------------------------------------------
 # SUBSYSTEMS -------------------------------------
 # ------------------------------------------------
 def subsystem_apt(user_defined):
-    os.environ['DEBIAN_FRONTEND'] = 'noninteractive'
+    os.environ["DEBIAN_FRONTEND"] = "noninteractive"
 
-    install_from_config('apt',
-                        lambda name: f'apt-get install -y --no-install-recommends {name}')
+    install_from_config(
+        "apt",
+        lambda name: f"apt-get install -y --no-install-recommends {name}",
+        user_defined,
+    )
+
 
 def subsystem_lua(user_defined):
-    install_from_config('lua',
-                        lambda name: f'luarocks install {name}')
+    install_from_config("lua", lambda name: f"luarocks install {name}", user_defined)
+
 
 def subsystem_jupyter(user_defined):
-    install_from_config('jupyter',
-                        lambda name: f'jupyter nbextension enable {name}')
+    install_from_config(
+        "jupyter", lambda name: f"jupyter nbextension enable {name}", user_defined
+    )
+
 
 def subsystem_jupyterlab(user_defined):
-    install_from_config('jupyterlab',
-                        lambda name: f'jupyter labextension install {name}')
+    install_from_config(
+        "jupyterlab", lambda name: f"jupyter labextension install {name}", user_defined
+    )
+
+
 def subsystem_pip(user_defined):
-    install_from_config('pip',
-                        lambda name: f'pip --no-cache-dir install --upgrade {name}')
+    install_from_config(
+        "pip", lambda name: f"pip --no-cache-dir install --upgrade {name}", user_defined
+    )
+
 
 # ================================================
 # MAIN ===========================================
@@ -56,20 +70,21 @@ def subsystem_pip(user_defined):
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('type')
-    parser.add_argument('subsystem')
+    parser.add_argument("type")
+    parser.add_argument("subsystem")
 
     args = parser.parse_args()
 
     subsystems = {
-        'apt': subsystem_apt,
-        'lua': subsystem_lua,
-        'jupyter': subsystem_jupyter,
-        'jupyterlab': subsystem_jupyterlab,
-        'pip': subsystem_pip
+        "apt": subsystem_apt,
+        "lua": subsystem_lua,
+        "jupyter": subsystem_jupyter,
+        "jupyterlab": subsystem_jupyterlab,
+        "pip": subsystem_pip,
     }
 
-    subsystems[args.subsystem](args.type == 'user')
+    subsystems[args.subsystem](args.type == "user")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
